4ea87ead04e9fc088490ef03005e8d820960252a
hoangvuvan0611 <113603055+hoangvuvan0611@users.noreply.github.com>
2024-04-07 10:13:08 +0700

    Initial commit

---
+++ src/main/java/com/spring/javacv_attendance_recognition/JavacvAttendanceRecognitionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.javacv_attendance_recognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacvAttendanceRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacvAttendanceRecognitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/javacv_attendance_recognition/JavacvAttendanceRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.javacv_attendance_recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacvAttendanceRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
